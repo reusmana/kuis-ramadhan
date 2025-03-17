0085c25bae17fa3b9e5eb8b005ee871c8778b4dc
reusmana <reusmanasujani@gmail.com>
2025-03-17 13:48:01 +0700

    add button reset

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ document.addEventListener("DOMContentLoaded", function () {
   const playButtonHome = document.getElementById("playbuttonhome");
   const playButton = document.getElementById("playbutton");
   const overlay = document.getElementById("overlay");
+  const reset = document.getElementById("resetStorage");
   //   const audio = document.getElementById("audio");
 
   playButtonHome.addEventListener("click", function () {
     console.log("clicked");
     overlay.classList.remove("hidden");
     //   audio.play();
+  });
+
+  reset.addEventListener("click", function () {
+    localStorage.clear();
+    window.location.reload();
   });
 });
 
